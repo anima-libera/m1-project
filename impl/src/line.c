f51485c30ddb03b7550a3a5b9b3151e009777698
anima-libera <anim.libera@gmail.com>
2021-09-24 13:32:26 +0200

    Added an other line drawing algorithm just to see

--- impl/src/line.c
+++ impl/src/line.c
@@ -24,7 +24,10 @@ void line_naive(pg_t pg, pixel_t color,
 	int ix_new;
 	int iy_new;
 	float square_dist_new;
-	pg.pixel_grid[ix + pg.w * iy] = color;
+	if (0 <= ix && ix < (int)pg.w && 0 <= iy && iy < (int)pg.h)
+	{
+		pg.pixel_grid[ix + pg.w * iy] = color;
+	}
 	while (1)
 	{
 		while ((ix_new = roundf(x)), (iy_new = roundf(y)),
@@ -41,6 +44,66 @@ void line_naive(pg_t pg, pixel_t color,
 			break;
 		}
 		square_dist = square_dist_new;
-		pg.pixel_grid[ix + pg.w * iy] = color;
+		if (0 <= ix && ix < (int)pg.w && 0 <= iy && iy < (int)pg.h)
+		{
+			pg.pixel_grid[ix + pg.w * iy] = color;
+		}
+	}
+}
+
+void line_naive_2(pg_t pg, pixel_t color,
+	float xa, float ya, float xb, float yb)
+{
+	const float dx = xb - xa;
+	const float dy = yb - ya;
+	const float square_len = dx*dx + dy*dy;
+	const float len = sqrtf(square_len);
+	assert(len != 0.0f);
+
+	const float ndx = dx / len;
+	const float ndy = dy / len;
+	const float step = 0.01f;
+
+	#define FACTOR 0.01f
+	
+	float x = xa;
+	float y = ya;
+	int ix = roundf(x);
+	int iy = roundf(y);
+	float square_dist = (x-xb)*(x-xb) + (y-yb)*(y-yb);
+	int ix_new;
+	int iy_new;
+	float square_dist_new;
+	while (1)
+	{
+		while ((ix_new = roundf(x)), (iy_new = roundf(y)),
+			ix_new == ix && iy_new == iy)
+		{
+			if (0 <= ix && ix < (int)pg.w && 0 <= iy && iy < (int)pg.h)
+			{
+				pg.pixel_grid[ix + pg.w * iy].r =
+					(1.0f - FACTOR) * pg.pixel_grid[ix + pg.w * iy].r +
+					FACTOR * color.r;
+				pg.pixel_grid[ix + pg.w * iy].g =
+					(1.0f - FACTOR) * pg.pixel_grid[ix + pg.w * iy].g +
+					FACTOR * color.g;
+				pg.pixel_grid[ix + pg.w * iy].b =
+					(1.0f - FACTOR) * pg.pixel_grid[ix + pg.w * iy].b +
+					FACTOR * color.b;
+				pg.pixel_grid[ix + pg.w * iy].a =
+					(1.0f - FACTOR) * pg.pixel_grid[ix + pg.w * iy].a +
+					FACTOR * color.a;
+			}
+			x += ndx * step;
+			y += ndy * step;
+		}
+		ix = ix_new;
+		iy = iy_new;
+		square_dist_new = (x-xb)*(x-xb) + (y-yb)*(y-yb);
+		if (square_dist_new >= square_dist)
+		{
+			break;
+		}
+		square_dist = square_dist_new;
 	}
 }
--- impl/src/line.h
+++ impl/src/line.h
@@ -9,4 +9,7 @@
 void line_naive(pg_t pg, pixel_t color,
 	float xa, float ya, float xb, float yb);
 
+void line_naive_2(pg_t pg, pixel_t color,
+	float xa, float ya, float xb, float yb);
+
 #endif /* HEADER_LINE_ */
