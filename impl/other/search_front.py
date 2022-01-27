f8ab606cb403c4a0e1026b185ad0b366198bf396
anima-libera <anim.libera@gmail.com>
2022-01-27 12:20:14 +0100

    Better front search

--- impl/other/search_front.py
+++ impl/other/search_front.py
@@ -9,8 +9,8 @@ os.chdir("bin")
 TIMEOUT = 100
 ERROR_MAX = 0.36
 
-some_param_value_table = [
-	1, 0, 0, 0, 0, 0.0001,
+some_good_param_value_table = [
+	1.0, 0.0, 0.0, 0.0, 0.0, 0.0001,
 	4000, 400, 0.6, 2, 200]
 
 initial_param_space_table = [ # [min, max, is_int]
@@ -51,10 +51,20 @@ def random_param_table(param_space_table):
 def mutate_param_table(param_value_table, param_space_table):
 	assert param_table_is_correct(param_value_table, param_space_table)
 	def mutate_param_table_maybe_incorrect(param_value_table, param_space_table):
-		TODO
-	new_param_value_table = mutate_param_table_maybe_incorrect(param_value_table, param_space_table)
-	while not param_table_is_correct(new_param_value_table):
-		new_param_value_table = mutate_param_table_maybe_incorrect(param_value_table, param_space_table)
+		return tuple(
+			param_value_table[i] if random.randint(1, 2) == 1 else
+				(param_value_table[i] + random.randint(-5, 5)
+				if param_space_table[i][2]
+				else param_value_table[i] + random.uniform(
+					-(param_space_table[i][1] - param_space_table[i][0]) * 0.1,
+					+(param_space_table[i][1] - param_space_table[i][0]) * 0.1))
+			for i in range(len(param_value_table))
+		)
+	new_param_value_table = mutate_param_table_maybe_incorrect(
+		param_value_table, param_space_table)
+	while not param_table_is_correct(new_param_value_table, param_space_table):
+		new_param_value_table = mutate_param_table_maybe_incorrect(
+			param_value_table, param_space_table)
 	return new_param_value_table
 
 # result = (time, error, line_count) or None in case of timeout
@@ -67,7 +77,8 @@ def stringart(param_value_table):
 			"--print-time-and-error",
 			"--no-output")
 	try:
-		output = subprocess.check_output(command(param_value_table), timeout=TIMEOUT).decode("ascii").splitlines()[-1].split()
+		output = subprocess.check_output(
+			command(param_value_table), timeout=TIMEOUT).decode("ascii").splitlines()[-1].split()
 		time = float(output[0])
 		error = float(output[1])
 		line_count = int(output[2])
@@ -83,35 +94,75 @@ def best_output(a, b):
 	else:
 		return None
 
+last_results = dict()
 try:
-	input_output_table = dict(open("results", "r").read())
-except:
-	input_output_table = dict()
+	last_results = eval(open("results", "r").read())
+	if len(last_results) >= 1:
+		print(f"Will load the {len(last_results)} last results first")
+except Exception as e:
+	print(e)
+
 input_output_table = dict()
 best_points = set()
 try:
 	while True:
-		plt.axis([0, TIMEOUT, 0, ERROR_MAX])
+
 		points = tuple(filter(lambda x: x != None, input_output_table.values()))
 		times = tuple(map(lambda v: v[0], points))
 		errors = tuple(map(lambda v: v[1], points))
-		plt.scatter(times, errors, c="orange")
 		best_points_values = tuple(map(lambda x: input_output_table[x], best_points))
 		best_times = tuple(map(lambda v: v[0], best_points_values))
 		best_errors = tuple(map(lambda v: v[1], best_points_values))
+
+		plt.axis([0, TIMEOUT, 0, ERROR_MAX])
+		plt.scatter(times, errors, c="orange")
 		plt.scatter(best_times, best_errors, c="black")
 		plt.savefig("search_front.png", dpi = 100)
 
-		param_value_table = random_param_table(param_space_table)
+		plt.axis([0, 60, 0, 0.05])
+		plt.scatter(times, errors, c="orange")
+		plt.scatter(best_times, best_errors, c="black")
+		plt.savefig("search_front_zoom.png", dpi = 100)
 
-		time = TIMEOUT
+		if len(last_results) >= 1:
+			some_last_result = last_results.popitem()
+			if len(last_results) == 0:
+				print("Loading the last previous result")
+			param_value_table = some_last_result[0]
+			output = some_last_result[1]
+			time = output[0] if output != None else TIMEOUT
+
+		else:
+			if tuple(some_good_param_value_table) not in input_output_table.keys():
+				param_value_table = tuple(some_good_param_value_table)
+
+			elif random.randint(1, 100) <= 60 and len(best_points) >= 1:
+				min_error = 10 * ERROR_MAX
+				min_error_best_point = None
+				for best_point in best_points:
+					if input_output_table[best_point][1] < min_error:
+						min_error = input_output_table[best_point][1]
+						min_error_best_point = best_point
+				best_point_to_mutate = (min_error_best_point if min_error_best_point != None
+					else random.choice(tuple(best_points)),)[0]
+				param_value_table = mutate_param_table(best_point_to_mutate, param_space_table)
+			elif random.randint(1, 100) <= 30 and len(best_points) >= 1:
+				best_point_to_mutate = random.choice(tuple(best_points))
+				param_value_table = mutate_param_table(best_point_to_mutate, param_space_table)
+			else:
+				param_value_table = random_param_table(param_space_table)
+
+			time = TIMEOUT
+			output = stringart(param_value_table)
+
+		print(param_value_table)
+		
 		count = len(input_output_table)
-		output = stringart(param_value_table)
 		if output != None:
 			time = output[0]
 			if output[1] > ERROR_MAX:
 				output = None
-				print(f"[{count:06}] ({time}) {output}")
+				print(f"[{count:06}] ({time}) error too high")
 			else:
 				print(f"[{count:06}] {output}")
 		else:
@@ -132,6 +183,11 @@ try:
 			if is_best_point:
 				best_points.difference_update(beaten_points)
 				best_points.add(param_value_table)
+				print("New best points:")
+				for best_point in best_points:
+					print(f"[{input_output_table[best_point]}]",
+						"--coefs", *map(str, best_point[:6]),
+						"--params", *map(str, best_point[6:]))
 
 except KeyboardInterrupt:
 	print()
